86b0060bf0c18732339651e9014c610a02f6dad1
Nazar Kobernyk <kogito.nk@gmail.com>
2025-05-29 17:08:03 +0100

    Add Supabase dependencies, update AppLayout import, and refactor UI components

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,16 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function() {
+    async function fetchCabins() {
+      const cabins = await getCabins();
+      console.log(cabins);
+    }
+    fetchCabins();
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
