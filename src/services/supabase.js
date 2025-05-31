4030e4deb24e4d583bbe1a5561f59a1e53e8feb8
Nazar Kobernyk <kogito.nk@gmail.com>
2025-05-31 18:13:21 +0100

    Enhance CreateCabinForm with react-hook-form integration, add image upload functionality, and improve error handling. Update CabinRow to support cross-origin images and modify AppLayout for scroll overflow. Refactor FileInput to set type attribute directly.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://zikaiaoqxxdyxywvehzm.supabase.co";
+export const supabaseUrl = "https://zikaiaoqxxdyxywvehzm.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppa2FpYW9xeHhkeXh5d3ZlaHptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTQwOTUsImV4cCI6MjA2NDAzMDA5NX0.nsnnZXZst9rwSSoJmARpiJIfbjsUxg5OVVBoBu2kjSI";
 const supabase = createClient(supabaseUrl, supabaseKey);
