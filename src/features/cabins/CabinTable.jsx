01e5afb586a93d3095d2125572febe28e903b337
Nazar Kobernyk <nazarkobernyk@MacBook-Pro-Nazar.local>
2025-10-31 17:42:40 +0000

    Refactor menus with context and portals; integrate new menus into cabin table and rows.

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import {useCabins} from "./useCabins";
 import Table from "../../ui/Table.jsx";
+import Menus from "../../ui/Menus.jsx";
 
 //
 // const TableHeader = styled.header`
@@ -28,19 +29,21 @@ function CabinTable() {
     if (error) return <p>Error loading cabins</p>;
 
     return (
-        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-            <Table.Header>
-                <div></div>
-                <div>Cabin</div>
-                <div>Capacity</div>
-                <div>Price</div>
-                <div>Discount</div>
-                <div></div>
-            </Table.Header>
-            {cabins.map((cabin) => (
-                <CabinRow key={cabin.id} cabin={cabin}/>
-            ))}
-        </Table>
+        <Menus>
+            <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+                <Table.Header>
+                    <div></div>
+                    <div>Cabin</div>
+                    <div>Capacity</div>
+                    <div>Price</div>
+                    <div>Discount</div>
+                    <div></div>
+                </Table.Header>
+                {cabins.map((cabin) => (
+                    <CabinRow key={cabin.id} cabin={cabin}/>
+                ))}
+            </Table>
+        </Menus>
     );
 }
 
